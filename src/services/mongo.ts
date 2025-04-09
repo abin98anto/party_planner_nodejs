8bbf8de39930656444f15057ae0cd49774047298
abin98anto <abin98anto@gmail.com>
2025-04-10 01:23:40 +0530

    modified the comment in mongo.ts

--- src/services/mongo.ts
+++ src/services/mongo.ts
@@ -5,7 +5,7 @@ dotenv.config();
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI as string);
-    console.log(`MongoDB connected: ${conn.connection.host}`);
+    console.log(`MongoDB connected`);
   } catch (error) {
     console.error("MongoDB connection error:", error);
     process.exit(1);
