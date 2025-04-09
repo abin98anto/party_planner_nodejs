d9d904edd4fe2d5e7dd0dad5f10fcfece2d4d65a
abin98anto <abin98anto@gmail.com>
2025-04-10 01:06:44 +0530

    added mongo config to application.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { connectDB } from "./services/mongo";
 
 const app = express();
 const PORT = 5000;
@@ -8,5 +9,7 @@ app.get("/", (_req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  connectDB().then(() => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
 });
