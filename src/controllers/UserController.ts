40bfd886164bc281d35ed5d0b38e01d53f108351
abin98anto <abin98anto@gmail.com>
2025-04-10 15:53:23 +0530

    added constants file.

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -5,13 +5,19 @@ import IUser from "../types/IUser";
 import hashPassword from "../utils/HashPassword";
 
 const signup = async (req: Request, res: Response) => {
-  const userData: IUser = req.body;
+  try {
+    const userData: IUser = req.body;
 
-  const userExists = await UserModel.findOne({ email: userData.email });
-  if (userExists) {
-    return res.status(409).json({ message: "Email is taken." });
+    const userExists = await UserModel.findOne({ email: userData.email });
+    if (userExists) {
+      return res.status(409).json({ message: "Email is taken." });
+    }
+
+    const hashedPassword = hashPassword(userData.password);
+    const newUser = new UserModel({ ...userData, password: hashedPassword });
+    await newUser.save();
+  } catch (error) {
+    console.log("error in user signup", error);
+    res.status(500).json();
   }
-
-  const hashedPassword = hashPassword(userData.password);
-  
 };
---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+const CNST = {
+  SERVER_ERR: "Internal Server Error",
+};
+
+export default CNST;
