07f883e2900f0736836820023dae36d2e8430991
abin98anto <abin98anto@gmail.com>
2025-04-10 19:28:38 +0530

    added user router in index.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+
 import { connectDB } from "./services/mongo";
+import userRouter from "./routes/UserRouter";
 
 const app = express();
+app.use(cookieParser());
+app.use(express.json());
+
+app.use("/", userRouter);
+
 const PORT = 5000;
-
-app.get("/", (_req, res) => {
-  res.send("Hello from TypeScript + TSX!");
-});
-
 app.listen(PORT, () => {
   connectDB().then(() => {
     console.log(`Server is running on http://localhost:${PORT}`);
