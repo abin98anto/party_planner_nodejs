e206fe6e74fa0d5694ddd0c76639cad3f7cf50f6
abin98anto <abin98anto@gmail.com>
2025-04-10 14:26:03 +0530

    added address modal.

--- src/models/AddressModel.ts
+++ src/models/AddressModel.ts
@@ -1,11 +1,19 @@
 import mongoose from "mongoose";
 
-const AddressSchema = new mongoose.Schema({
-  venue: String,
-  place: String,
-  landmark: String,
-  city: String,
-  district: String,
-  State: String,
-  pincode: Number,
-});
+const AddressSchema = new mongoose.Schema(
+  {
+    venue: String,
+    place: String,
+    landmark: String,
+    city: String,
+    district: String,
+    State: String,
+    pincode: Number,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const AddressModel = mongoose.model("Address", AddressSchema);
+export default AddressModel;
