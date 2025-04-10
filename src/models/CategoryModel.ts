d1214370ae536ce879f2848a52d176811dc17fbf
abin98anto <abin98anto@gmail.com>
2025-04-10 14:15:43 +0530

    added cateogory model.

--- src/models/CategoryModel.ts
+++ src/models/CategoryModel.ts
@@ -1,6 +1,14 @@
 import mongoose from "mongoose";
 
-const RatingSchema = new mongoose.Schema({
-  name: String,
-  isActive: String,
-});
+const CategorySchema = new mongoose.Schema(
+  {
+    name: String,
+    isActive: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const CategoryModel = mongoose.model("Category", CategorySchema);
+export default CategoryModel;
