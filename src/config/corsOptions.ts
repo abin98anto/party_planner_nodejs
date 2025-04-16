36c20430e941a708493406e0b70840d11ff555af
abin98anto <abin98anto@gmail.com>
2025-04-16 09:16:54 +0530

    cors options changed to vercel

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -1,5 +1,5 @@
 export const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173", "https://party-planner-six.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
